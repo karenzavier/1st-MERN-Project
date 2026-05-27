5bcd324168ee56f6d42c611c0ff62d7f91844999
Karen Zavier <karensheronezavier@gmail.com>
2026-05-27 22:02:28 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
+
+
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      
+<Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+       <Route path='/collection' element={<Collection/>}/>
+       <Route path='/about' element={<About/>}/>
+       <Route path='/contact' element={<Contact/>}/>
+       
+    
+       
+
+
+
+
+
+
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="p-10 text-2xl">
+      Contact Page Working
+    </div>
+  )
+}
+
+export default Contact
