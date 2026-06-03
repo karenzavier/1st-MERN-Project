c6b4b9ed815c1181ba420fda1f72a608f1d77d75
Karen Zavier <karensheronezavier@gmail.com>
2026-06-03 08:41:21 +0530

    Added Hero section and ShopContext setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+
 export default function Home() {
   return (
     <div>
-      
+      <Hero/>
+<LatestCollection/>
+
     </div>
   )
 }
